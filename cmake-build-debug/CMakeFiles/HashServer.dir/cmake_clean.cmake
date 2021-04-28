34116c9803acd2c2eeac10ce198fb9a33dc182b9
CyrusTheVirus <cyrusmajd2001@gmail.com>
2021-04-28 00:56:11 -0400

    dependancies init

---
+++ cmake-build-debug/CMakeFiles/HashServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HashServer.dir/main.c.o"
+  "HashServer"
+  "HashServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HashServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
